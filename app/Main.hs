66cb1ad0245b0fb5400561bbf536784c220fa3c3
Marcus Rosti <marcus.rosti@sony.com>
2021-12-01 18:03:39 -0800

    Day 1 part 2

--- app/Main.hs
+++ app/Main.hs
@@ -1,9 +1,7 @@
 module Main where
 
-import Lib
 import Day.One.DayOne
-
 
 main :: IO ()
 main = do
-    day1 "data/day1/input"
+  day1 "data/day1/input"
--- src/Day/One/DayOne.hs
+++ src/Day/One/DayOne.hs
@@ -1,21 +1,34 @@
 module Day.One.DayOne (day1) where
 
+import Data.List
+
 readLines :: String -> [Int]
 readLines = map stringToInt . lines
 
 stringToInt :: String -> Int
 stringToInt = read
 
-zipWithSelf :: [a] -> [(a, a)]
-zipWithSelf xs = zip (init xs) (tail xs)
-
-filterValues :: Ord a => [(a, a)] -> [(a, a)]
-filterValues = filter (uncurry (<))
+increasing :: Ord a => [a] -> Bool
+increasing [] = True
+increasing [_] = True
+increasing (a : b : xs) = a < b && increasing (b : xs)
 
 countOfIncreasingValues :: Ord a => [a] -> Int
-countOfIncreasingValues = length . filterValues . zipWithSelf
+countOfIncreasingValues = length . filter increasing . slidingWindow 2
+
+slidingWindow :: Int -> [a] -> [[a]]
+slidingWindow n xs = filter (\t -> length t == n) (transpose (take n (tails xs)))
+
+part1 :: [Int] -> Int
+part1 = countOfIncreasingValues
+
+part2 :: [Int] -> Int
+part2 = countOfIncreasingValues . map sum . slidingWindow 3
 
 day1 :: String -> IO ()
 day1 file = do
   content <- readFile file
-  print $ countOfIncreasingValues (readLines content)
+  let list = readLines content
+  putStrLn "Day 1"
+  print $ part1 list
+  print $ part2 list
